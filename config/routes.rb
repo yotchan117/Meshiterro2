ac2af2a2859af909cbfeda7726c5c780e7a267cd
EC2 Default User <yotchan32323@gmail.com>
2023-01-12 11:21:38 +0000

    [Add] いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "homes/about" => "homes#about", as: "about"
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
 end
