ceb11728a3f5ebaca0d5383d101fdb7de8ddcd39
EC2 Default User <yotchan32323@gmail.com>
2023-01-12 09:38:30 +0000

    [Add] 新規投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 end
