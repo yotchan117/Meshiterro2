a69ef153cbb4c59493564b6feab86f08bc604cd1
EC2 Default User <yotchan32323@gmail.com>
2023-01-12 09:27:12 +0000

    [Add] 画像投稿のコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
+  resources :post_images, only: [:new, :index, :show]
 end
